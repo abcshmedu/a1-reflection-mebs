4fc02656c196aed5f59040e112da51f33e1f07ea
Sebastian Baumann <bauman21@hm.edu>
2017-04-02 16:37:13 +0200

    Update latest changes in Renderer.java and RenderMe annotation.

--- reflection/src/main/java/edu/hm/cs/swa/reflection/RenderMe.java
+++ reflection/src/main/java/edu/hm/cs/swa/reflection/RenderMe.java
@@ -4,13 +4,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * This is an annotation-class named RenderMe
- * @author: Sebastian Baumann & Ehsan Moslehi
+ * This is an annotation-class named RenderMe.
+ * @author Sebastian Baumann & Ehsan Moslehi
  */
 
 @Retention(RetentionPolicy.SOURCE)  // sets retention of this annotation to source code like comments
 public @interface RenderMe {
 
+    /**
+     * First test-method.
+     * @return message as String
+     */
     String message() default "Annotation \"message\" is called successfully"; // this is a test-annotation. It will be removed in final version.
 
 }
--- reflection/src/main/java/edu/hm/cs/swa/reflection/Renderer.java
+++ reflection/src/main/java/edu/hm/cs/swa/reflection/Renderer.java
@@ -2,22 +2,27 @@ package edu.hm.cs.swa.reflection;
 
 /**
  * This is the class Renderer for an easy reflection.
- * @author: Sebastian Baumann & Ehsan Moslehi
+ * @author Sebastian Baumann & Ehsan Moslehi
  */
 
 public class Renderer {
 
     //TODO set object-variables
+    private int firstVariable = 0;
 
-    Renderer(Object o) {
+    Renderer(int i) {
         //TODO find out what this constructor does with the object as parameter. Which type of object is given as parameter?
+        this.firstVariable = i;
     }
 
     /* @RenderMe */
+    /**
+     * First implementation of public String render() method.
+     * @return message as String
+     */
     public String render() {
         //TODO return comfortable string representation like toString()-method
-        String s = "";
-        return s;
+        return String.valueOf(firstVariable);
     }
 
 }
