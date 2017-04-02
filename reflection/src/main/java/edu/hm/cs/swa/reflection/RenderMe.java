82ac6ca671a9daef5feaf191d0f9d64a8c28ba8c
Sebastian Baumann <bauman21@hm.edu>
2017-04-02 15:56:39 +0200

    Update changed RenderMe-Annotation, add new class-file Renderer.java.

--- reflection/src/main/java/edu/hm/cs/swa/reflection/RenderMe.java
+++ reflection/src/main/java/edu/hm/cs/swa/reflection/RenderMe.java
@@ -1,12 +1,16 @@
 package edu.hm.cs.swa.reflection;
 
-/**
- * This is a annotation-class named RenderMe
- * @author: Sebastian Baumann & Ehsan Moslehi
- * /
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
+/**
+ * This is an annotation-class named RenderMe
+ * @author: Sebastian Baumann & Ehsan Moslehi
+ */
+
+@Retention(RetentionPolicy.SOURCE)  // sets retention of this annotation to source code like comments
 public @interface RenderMe {
 
-
+    String message() default "Annotation \"message\" is called successfully"; // this is a test-annotation. It will be removed in final version.
 
 }
---
+++ reflection/src/main/java/edu/hm/cs/swa/reflection/Renderer.java
@@ -0,0 +1,23 @@
+package edu.hm.cs.swa.reflection;
+
+/**
+ * This is the class Renderer for an easy reflection.
+ * @author: Sebastian Baumann & Ehsan Moslehi
+ */
+
+public class Renderer {
+
+    //TODO set object-variables
+
+    Renderer(Object o) {
+        //TODO find out what this constructor does with the object as parameter. Which type of object is given as parameter?
+    }
+
+    /* @RenderMe */
+    public String render() {
+        //TODO return comfortable string representation like toString()-method
+        String s = "";
+        return s;
+    }
+
+}
